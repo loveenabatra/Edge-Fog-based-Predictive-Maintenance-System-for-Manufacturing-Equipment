4e5052b7f921df5acf9e892a53ac001ca12f1200
Lavisha-keshwani <lavishakeshwani03@gmail.com>
2026-03-09 19:42:31 +0530

    first commit

---
+++ edge device/edge_inference.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import joblib
+import json
+import time
+import paho.mqtt.client as mqtt
+
+model = joblib.load("predictive_model.pkl")
+
+THINGSBOARD_HOST = "thingsboard.cloud"
+ACCESS_TOKEN = "d00a9ohvqpbwczmaygb5"
+
+client = mqtt.Client()
+client.username_pw_set(ACCESS_TOKEN)
+client.connect(THINGSBOARD_HOST,1883,60)
+
+data = pd.read_csv("sensor_stream.csv")
+
+features = ["sensor2","sensor3","sensor4","sensor7","sensor11","sensor12","sensor15"]
+
+for index,row in data.iterrows():
+
+    input_data = row[features].values.reshape(1,-1)
+
+    prediction = model.predict(input_data)[0]
+
+    payload = {
+        "sensor2":float(row["sensor2"]),
+        "sensor3":float(row["sensor3"]),
+        "sensor4":float(row["sensor4"]),
+        "sensor7":float(row["sensor7"]),
+        "sensor11":float(row["sensor11"]),
+        "sensor12":float(row["sensor12"]),
+        "sensor15":float(row["sensor15"]),
+        "failure_prediction":int(prediction)
+    }
+
+    client.publish(
+        "v1/devices/me/telemetry",
+        json.dumps(payload)
+    )
+
+    print(payload)
+
+    time.sleep(2)
